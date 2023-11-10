e8941095338ce483bb14da0ea3dddc3aeac10617
Scorpyyy <scorpyboyz793@gmail.com>
2023-11-10 20:22:05 +0800

    heading updt

--- src/config.ts
+++ src/config.ts
@@ -2,5 +2,5 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'ARLX - ALBUM'
-export const SITE_DESCRIPTION = 'My Photo Album | Journal.'
+export const SITE_DESCRIPTION = 'My Photo Album | Journal |.'
 export const HOMEPAGE_URL = 'https://arlxxjamss.web.app/'
